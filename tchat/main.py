9fb8f5f955ebb0151505ff842a5090b42eb926ee
praveen <praveen.balasundram@gmail.com>
2025-12-18 20:22:32 +0530

    Format code

--- tchat/main.py
+++ tchat/main.py
@@ -5,12 +5,15 @@ from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_core.chat_history import InMemoryChatMessageHistory
 
 store = {}
+
+
 def get_session_history(session_id):
-    if session_id in store: 
+    if session_id in store:
         return store[session_id]
-    
+
     store[session_id] = InMemoryChatMessageHistory()
     return store[session_id]
+
 
 model = ChatOllama(model="llama3:8b")
 
@@ -18,7 +21,7 @@ prompt = ChatPromptTemplate(
     messages=[
         ("system", "You are a helpful assistant who speaks minimally"),
         ("placeholder", "{history}"),
-        ("human", "{content}")
+        ("human", "{content}"),
     ]
 )
 
@@ -27,19 +30,16 @@ chat_with_history = RunnableWithMessageHistory(
     runnable=chat_chain,
     get_session_history=get_session_history,
     input_messages_key="content",
-    history_messages_key="history"
+    history_messages_key="history",
 )
 
-while True: 
+while True:
     human_msg = input(">> ")
     ai_msg = chat_with_history.invoke(
         input={"content": human_msg},
-        config={"configurable": {"session_id": "bill-gates"}}
+        config={"configurable": {"session_id": "bill-gates"}},
     )
     print(ai_msg)
 
     chat_history = get_session_history("bill-gates")
     print(chat_history)
-
-
-
